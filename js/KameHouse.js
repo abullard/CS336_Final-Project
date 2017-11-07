932b664683785d135d7b2244102c40932d53e45b
Austin Bullard <abullard@iastate.edu>
2017-11-07 00:55:54 -0600

    Added the palm trees and method stubs

--- js/KameHouse.js
+++ js/KameHouse.js
@@ -8,8 +8,6 @@ var scene, camera, renderer;
 var width  = window.innerWidth;
 var height = window.innerHeight;
 
-var SPEED = 0.01;
-
 function init() {
 
   scene = new THREE.Scene();
@@ -17,34 +15,54 @@ function init() {
   initCamera();
   initLights();
   initRenderer();
+  // initWater();
+  // initReflections();
 
   document.body.appendChild(renderer.domElement);
 
   var render = function () {
       renderer.render(scene, camera);
-      rotateMesh();
       requestAnimationFrame(render);
   };
 
   render();
 }
 
-var mesh = null;
 function initMesh() {
   var loader = new THREE.JSONLoader();
-  loader.load('../models/shape.json', function(geometry, materials) {
-      mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
-      mesh.scale.x = mesh.scale.y = mesh.scale.z = 0.5;
-      scene.add(mesh);
+
+  // island model
+  loader.load('./models/island.json', function(geometry, materials) {
+      var island = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+      scene.add(island);
   });
+
+  // palm tree model
+  loader.load('./models/palmtree.json', function(geometry, materials) {
+      var tree = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+      tree.scale.x = tree.scale.y = tree.scale.z = 0.5;
+      tree.position.set(1, 1, 0);
+      scene.add(tree);
+
+      var tree2 = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+      tree2.scale.x = tree2.scale.y = tree2.scale.z = 0.5;
+      tree2.position.set(-1, 1, 0.5);
+      scene.add(tree2);
+  });
+
+  // kame house model
+
+  // bush model
+
+  // rock model
 }
 
 function initLights() {
     var light = new THREE.PointLight(0xffffff, 1.0);
-    light.position.set(0, 10, -5);
+    light.position.set(0, 20, -5);
+    scene.add(light);
 
     var ambientLight = new THREE.AmbientLight(0xffffff);
-    scene.add(light);
     scene.add(ambientLight);
 }
 
@@ -60,104 +78,10 @@ function initRenderer() {
   renderer.setClearColor(0xf0f0f0);
 }
 
-function rotateMesh() {
-    if (!mesh) {
-        return;
-    }
-
-    mesh.rotation.y -= SPEED;
-}
-
-// stuff for austin to do
-function austin() {
+function initWater() {
 
 }
 
-// stuff for dheeraj to do
-function dheeraj() {
+function initReflections() {
 
 }
-//
-// function start()
-// {
-//   // // Create a threejs renderer from the canvas
-//   // var ourCanvas = document.getElementById('theCanvas');
-//   // var renderer = new THREE.WebGLRenderer({canvas: ourCanvas});
-//   //
-//   // // create a scene and camera
-//   // var scene = new THREE.Scene();
-//   // var camera = new THREE.PerspectiveCamera( 30, 1.5, 0.1, 1000 );
-//   // camera.position.x = 2;
-//   // camera.position.y = 2;
-//   // camera.position.z = 5;
-//   // camera.lookAt(new THREE.Vector3(0, 0, 0));
-//   //
-//   // // Choose a geometry
-//   // //var geometry = new THREE.PlaneGeometry( 1, 1 );
-//   // var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-//   // //var geometry = new THREE.SphereGeometry(1);
-//   //
-//   // // Choose a material:
-//   // //var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-//   // //var material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
-//   // var material = new THREE.MeshPhongMaterial( { color: 0x00ff00, specular: 0x222222, shininess: 50} );
-//   //
-//   // // Note: we can make the PhongMaterial use face normals by selecting FlatShading
-//   // //var material = new THREE.MeshPhongMaterial( { color: 0x00ff00, specular: 0x222222, shininess: 50, shading: THREE.FlatShading} );
-//   //
-//   // // Create a mesh
-//   // var cube = new THREE.Mesh( geometry, material );
-//   //
-//   // // Add it to the scene
-//   // scene.add(cube);
-//   //
-//   //
-//   // // Make some axes, this will be a Line instead of a Mesh
-//   // var material = new THREE.LineBasicMaterial({color: 0xff0000});
-//   // var geometry = new THREE.Geometry();
-//   // geometry.vertices.push(
-//   //   new THREE.Vector3( 0, 0, 0 ),
-//   //   new THREE.Vector3( 2, 0, 0 )
-//   // );
-//   // var line = new THREE.Line( geometry, material );
-//   // scene.add( line );
-//   //
-//   // material = new THREE.LineBasicMaterial({color: 0x00ff00});
-//   // geometry = new THREE.Geometry();
-//   // geometry.vertices.push(
-//   //   new THREE.Vector3( 0, 0, 0 ),
-//   //   new THREE.Vector3( 0, 2, 0 )
-//   // );
-//   // line = new THREE.Line( geometry, material );
-//   // scene.add( line );
-//   //
-//   // material = new THREE.LineBasicMaterial({color: 0x0000ff});
-//   // geometry = new THREE.Geometry();
-//   // geometry.vertices.push(
-//   //   new THREE.Vector3( 0, 0, 0 ),
-//   //   new THREE.Vector3( 0, 0, 2 )
-//   // );
-//   // line = new THREE.Line( geometry, material );
-//   // scene.add( line );
-//   //
-//   // // Put a point light in the scene
-//   // var light = new THREE.PointLight(0xffffff, 1.0);
-//   // light.position.set(-2, 3, 5);
-//   // scene.add(light);
-//   //
-//   // // Put in an ambient light too
-//   // light = new THREE.AmbientLight(0x555555);
-//   // scene.add(light);
-//   //
-//   // // (**) If cube is not centered at origin, notice that 2nd and 3rd rotation
-//   // // examples do the rotation extrinsically
-//   // //cube.position.set(0.5, 0, 0);
-//
-//   var render = function () {
-//       renderer.render(scene, camera);
-//       var increment = 1.0 * Math.PI / 180.0;  // convert to radians
-//       requestAnimationFrame( render );
-//   };
-//
-//   render();
-// }
