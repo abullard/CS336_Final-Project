8ca1a69636cecbed5e7932b4f62a85da5d8b89d9
Austin Bullard <abullard@iastate.edu>
2017-11-28 17:04:37 -0600

    Working on kame house model now.

--- js/KameHouse.js
+++ js/KameHouse.js
@@ -32,14 +32,14 @@ function init() {
 
 function initSkybox(){
   scene.background = new THREE.CubeTextureLoader()
-    .setPath( 'images/test/dawnmountain-' )
+    .setPath( 'images/other/' )
     .load( [
-      'xpos.png',
-      'xneg.png',
-      'ypos.png',
-      'yneg.png',
-      'zpos.png',
-      'zneg.png'
+      'right.png',
+      'left.png',
+      'top.png',
+      'bottom.png',
+      'front.png',
+      'back.png'
     ]);
 }
 
@@ -50,19 +50,23 @@ function initMesh() {
   // island model
   loader.load('./models/island.json', function(geometry, materials) {
       var island = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+      island.scale.x = island.scale.y = island.scale.z = 0.88;
+      island.position.z = 2.3;
+      island.position.y = -0.4;
       scene.add(island);
   });
 
   // palm tree model
   loader.load('./models/palmtree.json', function(geometry, materials) {
+
       var tree = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
-      tree.scale.x = tree.scale.y = tree.scale.z = 0.5;
-      tree.position.set(1, 1, 0);
+      tree.scale.x = tree.scale.y = tree.scale.z = 0.3;
+      tree.position.set(1.6, 1, 1.6);
       scene.add(tree);
 
       var tree2 = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
-      tree2.scale.x = tree2.scale.y = tree2.scale.z = 0.5;
-      tree2.position.set(-1, 1, 0.5);
+      tree2.scale.x = tree2.scale.y = tree2.scale.z = 0.3;
+      tree2.position.set(-1, 1, 4.3);
       scene.add(tree2);
   });
 
@@ -71,6 +75,22 @@ function initMesh() {
   // bush model
 
   // rock model
+  loader.load('./models/rock.json', function(geometry, materials) {
+      var material = new THREE.MeshBasicMaterial( { color: 0x5c5d60 } );
+      var rock = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(material));
+      rock.scale.x = rock.scale.y = rock.scale.z = 0.2;
+      rock.position.set(-0.7, 1.5, 4.3);
+      scene.add(rock);
+  });
+
+  // fake water
+  var geometry = new THREE.PlaneGeometry(100, 100, 1, 1);
+  var material = new THREE.MeshPhongMaterial({color:0x00ccff, specular: 0x222222, shininess: 1});
+
+  var fakeWater = new THREE.Mesh(geometry, material);
+  fakeWater.position.y = 1;
+  fakeWater.rotation.x = -1.57;
+  scene.add(fakeWater);
 }
 
 function initLights() {
@@ -83,14 +103,14 @@ function initLights() {
 }
 
 function initCamera() {
-    camera = new THREE.PerspectiveCamera(70, width / height, 1, 10);
-    camera.position.set(0, 5, 8.5);
+    camera = new THREE.PerspectiveCamera(90, width / height, 1, 10);
+    camera.position.set(2.5, 3, 6.5);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
 }
 
 function initRenderer() {
-  var canvas = document.getElementById('theCanvas');
-  renderer = new THREE.WebGLRenderer({canvas: canvas});
+  renderer = new THREE.WebGLRenderer( {antialias: true} );
+  renderer.setSize(width, height);
   //renderer.setClearColor(0xf0f0f0);
 }
 
